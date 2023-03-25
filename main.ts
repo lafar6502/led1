1b49c052bd76e3fa4eb7a386b1777306591c9fe2
lafar6502 <rasputnik6502@gmail.com>
2023-03-25 17:01:16 +0100

    rainbow646

--- main.ts
+++ main.ts
@@ -1,46 +1,54 @@
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    serial.writeLine("aaa")
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.slide), SoundExpressionPlayMode.InBackground)
-    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-})
-input.onButtonPressed(Button.A, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    for (let col = 0; col <= 19; col++) {
-        serial.writeValue("x", col)
-        for (let row = 0; row <= 14; row++) {
-            strip.setMatrixColor(col, row, neopixel.colors(NeoPixelColors.Red))
-            serial.writeValue("y", row)
+function d1 () {
+    y = 15
+    for (let x = 0; x <= 19; x++) {
+        pix(strip, x, y, neopixel.rgb(0, 255, 0));
+for (let z = 1; z <= 10 && x - z >= 0; z++) {
+            pix(strip, x - z, y, neopixel.rgb(0, 250 - z * 25, 0));
         }
-        strip.show()
-        basic.pause(10)
-    }
-    for (let indeks = 0; indeks <= 10; indeks++) {
-        strip.setBrightness(255 - indeks * 25)
-        strip.show()
+strip.show()
         basic.pause(200)
     }
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4000; index++) {
+        strip.showRainbow(x2, x2 + 270)
+        x2 += 5
+        basic.pause(100)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 255, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    lista = []
-    for (let col = 0; col <= 300; col++) {
-        lista.push(neopixel.rgb(col % 255, col * 3 % 255, col * 5 % 255))
-    }
-    for (let indeks = 0; indeks <= lista.length; indeks++) {
-        strip.setPixelColor(indeks, lista[indeks])
-    }
-    strip.show()
+    pix(strip, 0, 0, neopixel.rgb(255, 0, 0));
+pix(strip, 1, 1, neopixel.rgb(120, 120, 0));
+pix(strip, 19, 0, neopixel.rgb(0, 255, 0));
+pix(strip, 19, 19, neopixel.rgb(0, 255, 255));
+pix(strip, 0, 19, neopixel.rgb(0, 0, 255));
+strip.show()
+    d1()
 })
-let lista: number[] = []
+let x2 = 0
 let strip: neopixel.Strip = null
+let y = 0
+function pix(s: neopixel.Strip, x: number, y: number, color: number) {
+    if (y % 2 == 1) {
+        x = 19 - x;
+    }
+    strip.setPixelColor(20 * y + x, color);
+}
 music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
-strip = neopixel.create(DigitalPin.P1, 300, NeoPixelMode.RGB)
-let range = strip.range(0, 300)
+basic.showLeds(`
+    . # . # .
+    # # # # #
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+strip = neopixel.create(DigitalPin.P0, 400, NeoPixelMode.RGB)
+let range = strip.range(0, 400)
 strip.setMatrixWidth(20)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 serial.redirectToUSB()
 serial.writeNumber(0)
-basic.forever(function () {
-	
-})
+x2 = 0
